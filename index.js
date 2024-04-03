70dd1a79e35b992fd50a0bd817fb2f156e45fe89
lizwachira <lizwanguwachira@gmail.com>
2024-04-03 16:22:41 +0300

    update arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 
-// Write your code here
+//variable called multiply, result of the multiplication should be 62. 
+let num1 = 62
+let num2 = 1
+let multiply = num1*num2
+console.log(multiply);//output 
+
+//variable that generates a random integer >0
+let random = Math.floor(Math.random() *7) + 1;
+console.log(random); //output
+
+//modulo operator (%) calculates the remainder 
+let num3 = 100
+let num4 = 6
+const mod = (num3%num4)
+console.log(mod); //output
+
+//highest number in a set
+const numbersSet = [2, 11, 20, 18, 35];
+const max = Math.max(...numbersSet);
+
+console.log(max); //output
